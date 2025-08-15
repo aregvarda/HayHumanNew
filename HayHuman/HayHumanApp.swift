0c04d0dc61353d91f4b0e0440fed10386c0d79d4
Areg Vardanian <91129638+aregvarda@users.noreply.github.com>
2025-08-15 18:11:32 +0300

    Initial Commit

---
+++ HayHuman/HayHumanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayHumanApp.swift
+//  HayHuman
+//
+//  Created by Арег Варданян on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct HayHumanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HayHumanTests/HayHumanTests.swift
@@ -0,0 +1,16 @@
+//
+//  HayHumanTests.swift
+//  HayHumanTests
+//
+//  Created by Арег Варданян on 15.08.2025.
+//
+
+import Testing
+
+struct HayHumanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
